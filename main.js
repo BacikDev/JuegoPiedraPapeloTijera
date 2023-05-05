34f2a88e30661492323f2c200806ebda19613611
BacikDev <bacik.dev@gmail.com>
2023-05-05 19:39:37 -0300

    Archivo JS con todas las funciones del juego terminado y optimizado

--- main.js
+++ main.js
@@ -2,38 +2,43 @@ function aleatorio(min, max){
     return Math.floor(Math.random()*(max-min+1)+ min)
 }
 
-let player = null
-let PC = aleatorio(1,3)
-player = prompt("Elige: 1 para piedra, 2 para papel o 3 para tijera")
-//aler("Elegiste " + player)
-
-//Player
-if(player == 1){
-    alert("Elegiste 🪨")
-}else if(player == 2){
-    alert("Elegiste 📃")
-}else if(player == 3){
-    alert("Elegiste ✂️")
-}else{
-    alert("Elegiste perder")
+function eleccion(jugada){
+    let resultado = ""
+    if(jugada == 1){
+        resultado = "🪨"
+    }else if(jugada == 2){
+        resultado = "📃"
+    }else if(jugada == 3){
+       resultado = "✂️"
+    }else{
+        resultado = "Mal elegido"
+    }
+    return resultado
 }
 
-//PC
-if(PC == 1){
-    alert("PC elige 🪨")
-}else if(PC == 2){
-    alert("PC elige 📃")
-}else if(PC == 3){
-    alert("PC elige ✂️")
-}else{
-    alert("PC elige perder")
+let player = 0
+let PC = 0
+let triunfos = 0
+let perdidas = 0
+
+while(triunfos < 3 && perdidas < 3){
+    PC = aleatorio(1,3)
+    player = prompt("Elige: 1 para piedra, 2 para papel o 3 para tijera")
+    //aler("Elegiste " + player)
+    
+    alert(`PC eligio: ${eleccion(PC)}`)
+    alert(`Tu eliges: ${eleccion(player)}`)
+    
+    //Combate
+    if(PC == player){
+        alert("Empate")
+    }else if((player == 1 && PC == 3) || (player == 2 && PC == 1) || (player == 3 && PC == 2)) {
+        alert("Ganaste")
+        triunfos++
+    }else {
+        alert("Perdiste")
+        perdidas++
+    }
 }
 
-//Combate
-if(PC == player){
-    alert("Empate")
-}else if((player == 1 && PC == 3) || (player == 2 && PC == 1) || (player == 3 && PC == 2)) {
-    alert("Ganaste")
-}else {
-    alert("Perdiste")
-}
+alert(`Ganaste ${triunfos} veces y perdiste ${perdidas} veces`)
